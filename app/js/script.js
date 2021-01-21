347935c579ccea4725b2db620d738e9c536ae8da
jehu12 <jehu1112@gmail.com>
2021-01-21 21:51:36 +0800

    second push

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,28 @@
-console.log('Hello');
+const btnHamburger = document.getElementById('hamburger');
+const header = document.querySelector('.header');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+const menu = () => {
+    console.log('Clicked');
+    header.classList.toggle('open');
+}
+
+const overlay = () => {
+    if(header.classList.contains('open')) {
+        fadeElems.forEach(element => {              
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    } else {
+        fadeElems.forEach(element => {    
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    }
+}
+
+
+btnHamburger.addEventListener('click', () => {
+    menu();
+    overlay();
+});
